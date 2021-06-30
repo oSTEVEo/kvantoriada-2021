86ac0742cdfa098007b4ddc5f335c2511004e1b4
oSTEVEo <steve.games@bk.ru>
2021-06-30 10:48:39 +0300

    Fix

--- Modules/GPS_Compass/GPS_Compass.ino
+++ Modules/GPS_Compass/GPS_Compass.ino
@@ -12,36 +12,45 @@ SoftwareSerial ss(RXPin, TXPin);
 //compass
 #include <Wire.h>
 #include <HMC5883L.h>
-#include <EEPROM.h>
 
 HMC5883L compass;
-//float MagnitCorrect = -0.69516464;
-long tmr = millis();
 
 #include "lib.h"
 #define RED A1
 #define GREEN A0
-#define BUTTON A3   
+#define BUTTON A3
 
 void setup() {
   Serial.begin(9600);
   ss.begin(GPSBaud);
+
+  Serial.begin(9600);
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+
+  // Initialize Initialize HMC5883L
   while (!compass.begin())
   {
-    Serial.println("Could not find a valid HMC5883L sensor, check wiring!");
     delay(500);
   }
 
+  // Set measurement range
   compass.setRange(HMC5883L_RANGE_1_3GA);
-  compass.setMeasurementMode(HMC5883L_CONTINOUS);                   // Set measurement mode
-  compass.setDataRate(HMC5883L_DATARATE_30HZ);                      // Set data rate
-  compass.setSamples(HMC5883L_SAMPLES_8);                           // Set number of samples averaged
 
-  compass.setOffset(eeprom_read_byte(5), eeprom_read_byte(10));     //calibration  (-651, 570)
+  // Set measurement mode
+  compass.setMeasurementMode(HMC5883L_CONTINOUS);
 
-  pinMode(RED, OUTPUT);
-  pinMode(GREEN, OUTPUT);
+  // Set data rate
+  compass.setDataRate(HMC5883L_DATARATE_30HZ);
+
+  // Set number of samples averaged
+  compass.setSamples(HMC5883L_SAMPLES_8);
   pinMode(BUTTON, INPUT_PULLUP);
+  delay(10);
+  if (!digitalRead(BUTTON) ) {
+    CalibrateCompass();
+  }
+  compass.setOffset((int)eeprom_read_word((const uint16_t*)5), (int)eeprom_read_word((const uint16_t*)10));
 }
 
 void loop() {
@@ -63,75 +72,67 @@ void loop() {
   printFloat(calculateCompass(), 1, 5, 6);
   Serial.println();
 
-  //callibrate Menu
-  if (digitalRead(BUTTON) && millis() - tmr > 100) {
-    calibrateCompass();
-    tmr = millis();
-  }
-
   smartDelay(1000);
 }
 
 float calculateCompass() {
   Vector norm = compass.readNormalize();
-  float heading = atan2(norm.YAxis, norm.XAxis);  // Calculate heading
 
+  // Calculate heading
+  float heading = atan2(norm.YAxis, norm.XAxis);
+  float declinationAngle = (4.0 + (26.0 / 60.0)) / (180 / M_PI);
+  heading += declinationAngle;
+
+  // Correct for heading < 0deg and heading > 360deg
   if (heading < 0)
+  {
     heading += 2 * PI;
-  if (heading > 2 * PI)
-    heading -= 2 * PI;
+  }
 
-  return heading * 180 / M_PI;;
+  if (heading > 2 * PI)
+  {
+    heading -= 2 * PI;
+  }
+
+  // Convert to degrees
+  return (float) (heading * 180 / M_PI);
 }
 
-void calibrateCompass() { //калибровка компаса//
-  digitalWrite(GREEN, 0);
-  for(int i = 0; i < 50; i++) {
-    digitalWrite(RED, 1);
-    delay(20);
-    digitalWrite(RED, 0);
-    delay(20);
-  }
-  
-  Vector mag = compass.readRaw();
 
-  int minX = mag.XAxis;
-  int maxX = mag.XAxis;
-  int minY = mag.YAxis;
-  int maxY = mag.YAxis;
+void CalibrateCompass() {
+  int minX = 0;
+  int maxX = 0;
+  int minY = 0;
+  int maxY = 0;
   int offX = 0;
   int offY = 0;
 
-  bool flag = 0;
-  long timer = millis();
+  Vector mag = compass.readRaw();
 
-  while (!digitalRead(BUTTON)) {
-    mag = compass.readRaw();
+  minX = mag.XAxis;
+  maxX = mag.XAxis;
+  minY = mag.YAxis;
+  maxY = mag.YAxis;
 
-    // Determine Min / Max values
+  for (int i = 0; i < 400; i++) {
+    Vector mag = compass.readRaw();
+
     if (mag.XAxis < minX) minX = mag.XAxis;
     if (mag.XAxis > maxX) maxX = mag.XAxis;
     if (mag.YAxis < minY) minY = mag.YAxis;
     if (mag.YAxis > maxY) maxY = mag.YAxis;
+    digitalWrite(RED, 1);
+    digitalWrite(GREEN, 1);
+    delay(20);
+    digitalWrite(RED, 0);
+    digitalWrite(GREEN, 0);
 
-    // Calculate offsets
-    offX = (maxX + minX) / 2;
-    offY = (maxY + minY) / 2;
-
-    if (millis() - timer > 450) {
-      if (flag) {
-        digitalWrite(RED, 0);
-        digitalWrite(GREEN, 1);
-      }
-      else {
-        digitalWrite(RED, 1);
-        digitalWrite(GREEN, 0);
-      }
-      timer = millis();
-      flag = !flag;
-    }
   }
-  EEPROM.update(5, offX);
-  EEPROM.update(10, offY);
-  compass.setOffset(EEPROM.read(5), EEPROM.read(10));
+
+  offX = (maxX + minX) / 2;
+  offY = (maxY + minY) / 2;
+  compass.setOffset(offX, offY);
+
+  eeprom_update_word(5, offX);
+  eeprom_update_word(10, offY);
 }
--- libs/LoRa/examples/LoRaReceiver/LoRaReceiver.ino
+++ libs/LoRa/examples/LoRaReceiver/LoRaReceiver.ino
@@ -7,7 +7,7 @@ void setup() {
 
   Serial.println("LoRa Receiver");
 
-  if (!LoRa.begin(915E6)) {
+  if (!LoRa.begin(433E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
